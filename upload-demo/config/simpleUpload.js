88e180bd289111d69927d525c3003a12c7f033b9
KouYidong <1223519172@qq.com>
2021-11-25 21:10:01 +0800

    fix: config

--- upload-demo/config/simpleUpload.js
+++ upload-demo/config/simpleUpload.js
@@ -1,48 +1,9 @@
+
 const os = require('os')
 const http = require('http')
 const https = require('https')
 const Axios = require('axios')
 const path = require('path')
-// 小文件路径
-// const filePath = '/Users/kouyidong/Documents/problem/20/upload-demo/物业关于摩托车、非机动车管理的通知.pdf'
-const filePath = '/Users/kouyidong/Documents/test/shoukuanma.png'
-
-// 大文件路径
-// const filePath = '/Users/kouyidong/Documents/problem/20/upload-demo/4.73 G的压缩包.zip'
-// const filePath = '/Users/kouyidong/Documents/test/100M图片.jpg'
-const sha256 = require("sha256")
-const fs = require('fs')
-const { v4: uuidv4 } = require('uuid');
-
-
-const sha256Str = sha256('beem')
-const appToken = `MbnLHUX_rmUJyI51S-G_d2XhvxZ94PB1fbndJUyG95qcOUoujwyppC8duYwuZqOgeZKLZbUxud0k`
-const uid = '13hLe_EB4hlb_LL7rOvH-1'
-const lastOcode = 'RAUC9MVR'
-const deviceId = 'com.beem.beemworkuat!2F8C32F6-5422-41B3-B673-547E35D38ACE'
-const beemLang = 'en-US'
-const version = '1.0.0-100'
-
-// const context = `http://120.92.86.2:9000`
-const context = `https://gateway-beemtest-1.rongcloud.net`
-const ver = `v1`
-
-
-const header = {
-  "Content-type": 'application/x-www-form-urlencoded',
-  "Authorization": `Beem ${appToken}`,
-  "app-ver": version,
-  uid,
-  "ocode": lastOcode,
-  // "device-id": deviceId,
-  'device-id': 'a0:78:17:67:f4:c6',
-  "device-type": os.type() === 'Darwin' ? '3' : '4',
-  "lang": beemLang,
-  "os-ver": '11',
-  "ts": Date.now(),
-  "ts-sign": '41e3d2a6bc1e2a5015a36edeaa9f6c72',
-  "nonce": Math.ceil(Math.random() * Math.pow(10, 10))
-}
 
 /**
  * 根据文件名称返回上传文件需要的 object
@@ -217,5 +178,4 @@ module.exports = {
   // 获取签名
   getSTCSign
 }
-
 
